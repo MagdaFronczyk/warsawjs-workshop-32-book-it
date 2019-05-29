bc83bcba214b4d6ef34ffecba82faa3763f7a4ea
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2019-05-29 19:35:56 +0200

    adds more stuff in details component

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const Details = (props) => {
-    const filteredById = props.data.find(el => el.id === props.match.params.id)
-    console.log(filteredById, props.data);
+
+    const { data, match } = props;
+
+    const hotel = data.find(el => el.id === match.params.id);
+
     return (
         <div>
-            <img src={filteredById.cover.url} alt="" />
+            <h2>{hotel.title}</h2>
+            <img src={hotel.cover.url} />
+            <h3>Demand: {hotel.demand}</h3>
+            <div>
+                <h3>Location</h3>
+                <p>{hotel.location.address}</p>
+            </div>
+            <div>
+                <h3>Price</h3>
+                <p>Price: {hotel.price.amount}{hotel.price.currency}</p>
+                <p>Breakfast: {hotel.price.breakfast ? 'included' : 'not included'}</p>
+            </div>
+            <div>
+                <h3>Rating</h3>
+                <p>Hotel rating average: {hotel.rating.average}</p>
+                <p>Total reviews: {hotel.rating.reviews}</p>
+            </div>
+            <div>
+                <h3>Room</h3>
+                <p>Room available: {hotel.room}</p>
+            </div>
         </div>
     )
 }
